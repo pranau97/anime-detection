9c3d7727f7bf841f666b67f0502184d083687f9c
Pranau Kumar <pranau@war-machine-mk2>
2018-04-04 04:12:03 +0530

    Code refactoring; Add support for reverse image lookup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Anime Character Recognition
+
+This is an OpenCV based system that uses a Haar Cascade Classifier to detect facial features of Japanese anime characters in a given image file.
--- detect.py
+++ detect.py
@@ -1,31 +1,75 @@
-import numpy as np
-import cv2
-import sys
+'''Python script to perform image detection and reverse image lookup.'''
+
 import os
+import sys
+import json
+import logging
+import cv2
+from saucenao.saucenao import SauceNao
 
-if len(sys.argv) != 2:
-	print('Usage: detect <input-file>')
-	sys.exit(0)
+DB = {'3.xml': 'Hunter x Hunter',
+      '2.xml': 'Hunter x Hunter',
+      '5.xml': 'Death Note',
+      '4.xml': 'Donald Duck Show',
+      '1.xml': 'Rick and Morty'}
 
-img = cv2.imread(sys.argv[1])
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+CHARACTER_DB = {'3.xml': 'Gon',
+                '2.xml': 'Killua',
+                '5.xml': 'L Death Note',
+                '4.xml': 'Donald',
+                '1.xml': 'Morty'}
 
-cascade_dir = 'cascades'
 
-db = {'3.xml':'Hunter x Hunter', '2.xml': 'Hunter x Hunter', '5.xml': 'Death Note', '4.xml': 'Donald Duck Show', '1.xml': 'Rick and Morty'}
+def detect_characters(img_path):
+    '''Reads the input image to do the following -
+        * Detect an anime character from the trained database of characters.
+        * Pass the image to cloud library to get visually similar images based on the content of the image.
+        * Scrape more images based on the character detected.
+    '''
 
-flag = 0
-for file in os.listdir(cascade_dir):
-	cascade = cv2.CascadeClassifier(cascade_dir+'/'+str(file))
-	detect = cascade.detectMultiScale(gray, 1.3, 5, 0, (100,100))
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-	for (x,y,w,h) in detect:
-		print('Series: '+db[str(file)])
-		cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-		flag = 1
-	if flag == 1:
-		break
+    flag = 0
+    for file in os.listdir('cascades'):
+        cascade = cv2.CascadeClassifier('cascades/' + str(file))
+        detect = cascade.detectMultiScale(gray, 1.3, 5, 0, (100, 100))
 
-cv2.imshow('Image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        for (x, y, w, h) in detect:
+            print('Series: ' + DB[str(file)])
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            flag = 1
+
+            api_data = json.load(open('api_key.json'))
+            sauce = SauceNao('', databases=999, minimum_similarity=65, combine_api_types=False,
+                             api_key=api_data['saucenao_key'],
+                             exclude_categories='', move_to_categories=False, output_type=SauceNao.API_HTML_TYPE, start_file=None,
+                             log_level=logging.ERROR, title_minimum_similarity=90)
+
+            data = {
+                "Records": [{"keywords": CHARACTER_DB[str(file)], "limit": 5, "print_urls": True}]
+            }
+
+            results = sauce.check_file(img_path)
+
+            print(results)
+            print("Evaluating image...")
+
+            with open('config.json', 'w') as outfile:
+                json.dump(data, outfile)
+
+            os.system("googleimagesdownload -cf config.json > /dev/null")
+
+        if flag == 1:
+            break
+
+    cv2.imshow('Image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python3 detect.py /path/to/input/image')
+        sys.exit(0)
+    detect_characters(sys.argv[1])
